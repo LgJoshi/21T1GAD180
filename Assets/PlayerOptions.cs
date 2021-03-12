59468f0bcb3f86b2b14834e8bdd3b4f8126d4daf
Josh Luong <1021857@student.sae.edu.au>
2021-03-12 14:09:07 +1100

    Started work on player optioons script

--- Assets/PlayerOptions.cs
+++ Assets/PlayerOptions.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerOptions : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TextMeshProUGUI myText;
 
-    // Update is called once per frame
-    void Update()
-    {
+
+
+    void changeDialogue(string newText, string tag) {
         
     }
 }
