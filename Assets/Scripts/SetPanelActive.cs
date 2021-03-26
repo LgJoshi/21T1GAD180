ee3208873b43434fef4c91de2de12a440861e5a1
Zac <1019470@student.sae.edu.au>
2021-03-26 15:10:48 +1100

    Disabled game selection view by default, then enabled on game run.

---
+++ Assets/Scripts/SetPanelActive.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPanelActive : MonoBehaviour
+{
+	public GameObject canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetToActive();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	public void SetToActive()
+	{
+		canvas.gameObject.SetActive(true);
+	}
+}
