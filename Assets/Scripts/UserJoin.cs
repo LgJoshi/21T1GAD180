50226803f016a51ad99f4e1a0b279abfb5065a0f
Aaron McDougall <1021572@student.sae.edu.au>
2021-03-31 16:59:25 +1100

    UserJoin.cs tidy up

--- Assets/Scripts/UserJoin.cs
+++ Assets/Scripts/UserJoin.cs
@@ -6,62 +6,64 @@ using TMPro;
 
 public class UserJoin : MonoBehaviour
 {
+    //text box
     public TMP_Text userNameJoin;
+
+    //links user scripts
     public UserA userA;
     public UserB userB;
     public UserC userC;
 
+    //self explanatory
     public string userNameA;
     public string userNameB;
     public string userNameC;
 
-    public List<string> stuff;
-    public int listLength;
-    public int unCheck;
+    public List<string> joined;
 
     void Start()
     {
-        List<string> stuff = new List<string>();
+        //creates the list
+        List<string> joined = new List<string>();
        
+        //gets user's names
         userNameA = userA.GetMyName();
         userNameB = userB.GetMyName();
         userNameC = userC.GetMyName();     
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     public void userJoin()
     {
-
-            if (userA.GetJoin() == true && !stuff.Contains("Tim"))
-            {
-                stuff.Add(userNameA);
-                CheckStuff();
-            }
-            //else
-            if (userB.GetJoin() == true && !stuff.Contains("Jenny"))
-            {
-                stuff.Add(userNameB);
-                CheckStuff();
-            }
-            //else
-            if (userC.GetJoin() == true && !stuff.Contains("Sam"))
-            {
-                stuff.Add(userNameC);
-                CheckStuff();
-            }
-
-        
-
+        //checks userA join and if the list DOESN'T contain the name
+        if (userA.GetJoin() == true && !joined.Contains("Tim"))
+        {
+            //adds user to the join list, prints list to joined panel
+            joined.Add(userNameA);
+            CheckJoined();
+        }
+        //else
+        //checks userB join and if the list DOESN'T contain the name
+        if (userB.GetJoin() == true && !joined.Contains("Jenny"))
+        {
+            //adds user to the join list, prints list to joined panel
+            joined.Add(userNameB);
+            CheckJoined();
+        }
+        //else
+        //checks userC join and if the list DOESN'T contain the name
+        if (userC.GetJoin() == true && !joined.Contains("Sam"))
+        {
+            //adds user to the join list, prints list to joined panel
+            joined.Add(userNameC);
+            CheckJoined();
+        }     
     }   
 
-    void CheckStuff()
+    void CheckJoined()
     {
-        string text = string.Join("\n", stuff.ToArray());
+        //converts list to string, prints to joined panel
+        string text = string.Join("\n", joined.ToArray());
         userNameJoin.text = (text);
     }
 
