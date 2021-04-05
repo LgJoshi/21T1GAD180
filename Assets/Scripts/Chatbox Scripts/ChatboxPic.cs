660b9403983de2bdaea9ef85248f5548c59e1122
Josh Luong <1021857@student.sae.edu.au>
2021-04-05 15:15:55 +1000

    Rearranging and scene changes. Created scripts for chatbox history

---
+++ Assets/Scripts/Chatbox Scripts/ChatboxPic.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChatboxPic : MonoBehaviour
+{
+    public int historyNumber;
+
+    public Sprite[] userPics;
+    SpriteRenderer spriteRenderer;
+
+    float chatDelay;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        chatDelay = 0.5;
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        for( int i = 0;i < 3;i++ ) {
+            Instantiate(prefab, new Vector3(i * 2.0F, 0, 0), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator PlayText() {
+        foreach( Sprite s in userPics ) {
+            spriteRenderer.sprite = s;
+            yield return new WaitForSeconds(chatDelay);
+        }
+    }
+}
