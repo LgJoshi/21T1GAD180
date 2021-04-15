ba55eafe5d8f49810eff2eb86180c5a159de82da
Josh Luong <1021857@student.sae.edu.au>
2021-04-15 18:12:28 +1000

    Music + Dialogue BG added

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour {
+    //Music list variable for controlling which AudioSource component to play when needed
+    public AudioSource[] music;
+
+    void OnEnable() {
+        EventManager.StartEvent += PlayMusic;
+        EventManager.EndEvent += StopMusic;
+    }
+    void OnDisable() {
+        EventManager.StartEvent -= PlayMusic;
+        EventManager.EndEvent -= StopMusic;
+    }
+
+    void PlayMusic() {
+        StartCoroutine(PlayDelay());
+    }
+
+    IEnumerator PlayDelay() {
+        yield return new WaitForSeconds(1.5f);
+        music[0].Play();
+    }
+
+    void StopMusic() {
+        music[0].Stop();
+    }
+}
